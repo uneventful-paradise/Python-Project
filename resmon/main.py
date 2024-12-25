184c9c82e8302cef43897a60f69fb3940f94a6b9
dante <davidtofan7@gmail.com>
2024-12-25 16:59:03 +0200

    live graph for cpu

--- resmon/main.py
+++ resmon/main.py
@@ -1,54 +1,47 @@
 import time
 import psutil
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
 import collections
 import datetime
 
-cpu_usage_data = collections.deque(maxlen=5)
-cpu_freq_data = collections.deque(maxlen=5)
-mem_usage_data = collections.deque(maxlen=5)
-time_data = collections.deque(maxlen=5)
 
-def bar_display(cpu_usage, mem_usage, bars=50):
-    cpu_percent = (cpu_usage / 100.0)
-    mem_percent = (mem_usage / 100.0)
-    cpu_bar = '█' * (int(cpu_percent * bars)) + '-'*(bars - int(cpu_percent * bars))
-    mem_bar = '█' * (int(mem_percent * bars)) + '-'*(bars - int(mem_percent * bars))
-    print(f"CPU USAGE: |{cpu_bar}| {cpu_usage}%")
-    print(f"MEM USAGE: |{mem_bar}| {mem_usage}%")
-    print("\033[1A\x1b[2K", end="")
-    print("\033[1A\x1b[2K", end="")
+class ResGraph():
+    def __init__(self):
+        self.cpu_usage_data = collections.deque(maxlen=5)
+        self.cpu_freq_data = collections.deque(maxlen=5)
+        self.mem_usage_data = collections.deque(maxlen=5)
+        self.time_data = collections.deque(maxlen=5)
+        self.update_data()
 
-def update_data():
-    cpu_usage_data.append(psutil.cpu_percent())
-    cpu_freq_data.append(psutil.cpu_freq().current)
-    mem_usage_data.append(psutil.virtual_memory().percent)
-    # time_data.append(datetime.datetime.now().strftime("%H:%M:%S"))
-    time_data.append(datetime.datetime.timestamp(datetime.datetime.now()))
+        self.fig, self.cpu_ax = plt.subplots()
+        myFmt = mdates.DateFormatter('%H:%M:%S')
+        self.cpu_ax.xaxis.set_major_formatter(myFmt)
+        self.plot = self.cpu_ax.plot(self.time_data, self.cpu_usage_data, label='CPU USAGE')[0]
+        self.cpu_ax.set_ylim(0, 100)
+        # self.cpu_ax.set_xlim(self.time_data[0], self.time_data[-1])
 
-    # print(cpu_usage_data)
-    # print(cpu_freq_data)
-    # print(mem_usage_data)
 
-# while True:
-#     # print(psutil.cpu_percent())
-#     # print(psutil.virtual_memory().percent)
-#     # print(psutil.cpu_freq().current)
-#
-#     # bar_display(psutil.cpu_percent(), psutil.virtual_memory().percent)
-#     update_data(psutil.cpu_percent(), psutil.cpu_freq().current, psutil.virtual_memory().percent)
-#     time.sleep(1)
+    def update_data(self):
+        self.cpu_usage_data.append(psutil.cpu_percent())
+        self.cpu_freq_data.append(psutil.cpu_freq().current)
+        self.mem_usage_data.append(psutil.virtual_memory().percent)
+        # time_data.append(datetime.datetime.now().strftime("%H:%M:%S"))
+        self.time_data.append(datetime.datetime.now())
+
+    def animate(self):
+        while True:
+            self.update_data()
+            print(f'time: {self.time_data}; cpu_usage: {self.cpu_usage_data}')
+            self.plot.set_xdata(self.time_data)
+            self.plot.set_ydata(self.cpu_usage_data)
+            self.cpu_ax.set_xlim(self.time_data[0], self.time_data[-1])
+            self.fig.canvas.draw()
+            self.fig.canvas.flush_events()
+            time.sleep(0.5)
+            # self.after(1000, self.animate)
 
 plt.ion()
-graph = plt.plot(time_data, cpu_usage_data)[0]
-
-for i in range(0, 6):
-    update_data()
-
-    graph.set_xdata(time_data)
-    graph.set_ydata(cpu_usage_data)
-
-    plt.draw()
-    plt.pause(0.01)
-    time.sleep(1)
-
+graph = ResGraph()
+graph.animate()

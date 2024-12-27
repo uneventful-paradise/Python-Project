1b65f24ccf6f86c2b2c0f4a454409e440fca3422
dante <davidtofan7@gmail.com>
2024-12-27 17:23:49 +0200

    added network usage monitoring

--- resmon/main.py
+++ resmon/main.py
@@ -16,8 +16,8 @@ from tkinter import ttk
 # ax1.set_title('Sharing Y axis')
 # ax2.scatter(x, y)
 #https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.subplots.html
-# https://www.geeksforgeeks.org/how-to-update-a-plot-on-same-figure-during-the-loop/
-
+#https://www.geeksforgeeks.org/how-to-update-a-plot-on-same-figure-during-the-loop/
+#https://www.geeksforgeeks.org/how-to-embed-matplotlib-charts-in-tkinter-gui/
 
 #TODO: network montoring
 #TODO: split graphs in subgraphs
@@ -35,6 +35,8 @@ class GraphFrame(tk.Frame):
         self.cpu_freq_data = collections.deque(maxlen=5)
         self.mem_usage_data = collections.deque(maxlen=5)
         self.time_data = collections.deque(maxlen=5)
+        self.network_data = collections.deque(maxlen=5)
+        self.old_network_value = 0
         self.update_data()
 
         self.fig = plt.figure(figsize = (16, 16))
@@ -48,7 +50,7 @@ class GraphFrame(tk.Frame):
         self.cpu_usage_ax.xaxis.set_major_formatter(ax_format)
         self.cpu_freq_ax.xaxis.set_major_formatter(ax_format)
         self.mem_usage_ax.xaxis.set_major_formatter(ax_format)
-
+        self.network_usage_ax.xaxis.set_major_formatter(ax_format)
 
         self.cpu_usage_plot = self.cpu_usage_ax.plot(self.time_data, self.cpu_usage_data, color = 'blue', label='CPU USAGE', linestyle = 'solid', linewidth = 3)[0]
         self.mem_usage_plot = self.mem_usage_ax.plot(self.time_data, self.mem_usage_data, color = 'green', label='MEMORY USAGE', linestyle = 'solid', linewidth = 3)[0]
@@ -58,6 +60,8 @@ class GraphFrame(tk.Frame):
 
 
         self.cpu_freq_plot = self.cpu_freq_ax.plot(self.time_data, self.cpu_freq_data, label='CPU Frequency (MHz)', linestyle = 'solid', color='purple', linewidth = 3)[0]
+        self.network_usage_plot = self.network_usage_ax.plot(self.time_data, self.network_data, label = 'Network Usage(Kbs/s)', linestyle = 'solid', color='orange', linewidth = 3)[0]
+        self.network_usage_ax.set_ylim(0, 5000)
         self.cpu_freq_ax.set_ylim(0, 5000)
 
         self.cpu_usage_ax.set_xlabel('Time')
@@ -79,12 +83,20 @@ class GraphFrame(tk.Frame):
         self.canvas.get_tk_widget().pack(side='top', fill=tk.BOTH, expand=True)
         self.update_data()
 
+    def get_network_usage(self):
+        new_value = psutil.net_io_counters(nowrap=True).bytes_sent + psutil.net_io_counters(nowrap=True).bytes_recv
+        net_usage = (new_value - self.old_network_value) / 1024.0 * 8
+        return (net_usage, new_value)
+
     def update_data(self):
         self.cpu_usage_data.append(psutil.cpu_percent())
         self.cpu_freq_data.append(psutil.cpu_freq().current)
         self.mem_usage_data.append(psutil.virtual_memory().percent)
         # time_data.append(datetime.datetime.now().strftime("%H:%M:%S"))
         self.time_data.append(datetime.datetime.now())
+        (current_net_usage, new_network_usage) = self.get_network_usage()
+        self.network_data.append(current_net_usage)
+        self.old_network_value = new_network_usage
 
     def animate(self):
         # while True:
@@ -99,10 +111,14 @@ class GraphFrame(tk.Frame):
         self.cpu_freq_plot.set_xdata(self.time_data)
         self.cpu_freq_plot.set_ydata(self.cpu_freq_data)
 
+        self.network_usage_plot.set_xdata(self.time_data)
+        self.network_usage_plot.set_ydata(self.network_data)
+
         self.cpu_freq_ax.set_xlim(self.time_data[0], self.time_data[-1])
         self.cpu_usage_ax.set_xlim(self.time_data[0], self.time_data[-1])
         self.mem_usage_ax.set_xlim(self.time_data[0], self.time_data[-1])
         self.network_usage_ax.set_xlim(self.time_data[0], self.time_data[-1])
+        self.network_usage_ax.set_ylim(0, max(self.network_data)+1000)
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()

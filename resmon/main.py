6c4207b583c02e8d9af2a152ad93410aaa5b806d
dante <davidtofan7@gmail.com>
2024-12-29 12:55:36 +0200

    split graphs added scroll

--- resmon/main.py
+++ resmon/main.py
@@ -19,57 +19,61 @@ from tkinter import ttk
 #https://www.geeksforgeeks.org/how-to-update-a-plot-on-same-figure-during-the-loop/
 #https://www.geeksforgeeks.org/how-to-embed-matplotlib-charts-in-tkinter-gui/
 
-#TODO: network montoring
-#TODO: split graphs in subgraphs
 
 LARGE_FONT = ("Verdana", 12)
+data = {}
+for name in ("cpu_usage", "cpu_freq", "mem_usage", "disk_usage", "time", "network_data", "network_in", "network_out"):
+    data[name] = collections.deque(maxlen=5)
+data["old_network_value"] = 0
+
+
+def get_network_usage():
+    new_value = psutil.net_io_counters(nowrap=True).bytes_sent + psutil.net_io_counters(nowrap=True).bytes_recv
+    net_usage = (new_value - data["old_network_value"]) / 1024.0 * 8
+    return net_usage, new_value
+
+def update_data(data_type = "all"):
+    if (len(data["time"]) > 0 and datetime.datetime.now() > data["time"][-1] + datetime.timedelta(0, 1)) or data_type == "all":
+        data["time"].append(datetime.datetime.now())
+    if data_type == "cpu_usage" or data_type == "all":
+        data["cpu_usage"].append(psutil.cpu_percent())
+    if data_type == "cpu_freq" or data_type == "all":
+        data["cpu_freq"].append(psutil.cpu_freq().current)
+    if data_type == "mem_usage" or data_type == "all":
+        data["mem_usage"].append(psutil.virtual_memory().percent)
+    if data_type == "network_data" or data_type == "all":
+        (current_net_usage, new_network_usage) = get_network_usage()
+        data["network_data"].append(current_net_usage)
+        data["old_network_value"] = new_network_usage
 
 class GraphFrame(tk.Frame):
-    def __init__(self, parent):
+    def __init__(self, parent, color, title, label, ylim, ylabel, data_type):
+        self.color = color
+        self.title = title
+        self.label = label
+
+        self.ylim = ylim
+        self.ylabel = ylabel
+        self.data_type = data_type
+
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Example of Live Plotting", font=LARGE_FONT)
+        label = tk.Label(self, text=self.title, font=LARGE_FONT)
         label.pack(pady=10, padx=10, side='top')
 
 
-        self.cpu_usage_data = collections.deque(maxlen=5)
-        self.cpu_freq_data = collections.deque(maxlen=5)
-        self.mem_usage_data = collections.deque(maxlen=5)
-        self.time_data = collections.deque(maxlen=5)
-        self.network_data = collections.deque(maxlen=5)
-        self.old_network_value = 0
-        self.update_data()
-
-        self.fig = plt.figure(figsize = (16, 16))
-        self.cpu_usage_ax = self.fig.add_subplot(2, 2, 1)
-        self.cpu_freq_ax = self.fig.add_subplot(2, 2, 2)
-        self.mem_usage_ax = self.fig.add_subplot(2, 2, 3)
-        self.network_usage_ax = self.fig.add_subplot(2, 2, 4)
-
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot()
 
         ax_format = mdates.DateFormatter('%H:%M:%S')
-        self.cpu_usage_ax.xaxis.set_major_formatter(ax_format)
-        self.cpu_freq_ax.xaxis.set_major_formatter(ax_format)
-        self.mem_usage_ax.xaxis.set_major_formatter(ax_format)
-        self.network_usage_ax.xaxis.set_major_formatter(ax_format)
-
-        self.cpu_usage_plot = self.cpu_usage_ax.plot(self.time_data, self.cpu_usage_data, color = 'blue', label='CPU USAGE', linestyle = 'solid', linewidth = 3)[0]
-        self.mem_usage_plot = self.mem_usage_ax.plot(self.time_data, self.mem_usage_data, color = 'green', label='MEMORY USAGE', linestyle = 'solid', linewidth = 3)[0]
-        self.cpu_usage_ax.set_ylim(0, 100)
-        self.mem_usage_ax.set_ylim(0, 100)
-        # self.percentage_ax.set_xlim(self.time_data[0], self.time_data[-1])
+        self.ax.xaxis.set_major_formatter(ax_format)
+        print(f'{data["time"]} and {data[self.data_type]}')
+        self.plot = self.ax.plot(data["time"], data[self.data_type], color = self.color, label=self.label, linestyle = 'solid', linewidth = 3)[0]
+        self.ax.set_ylim(0, ylim)
 
 
-        self.cpu_freq_plot = self.cpu_freq_ax.plot(self.time_data, self.cpu_freq_data, label='CPU Frequency (MHz)', linestyle = 'solid', color='purple', linewidth = 3)[0]
-        self.network_usage_plot = self.network_usage_ax.plot(self.time_data, self.network_data, label = 'Network Usage(Kbs/s)', linestyle = 'solid', color='orange', linewidth = 3)[0]
-        self.network_usage_ax.set_ylim(0, 5000)
-        self.cpu_freq_ax.set_ylim(0, 5000)
-
-        self.cpu_usage_ax.set_xlabel('Time')
-        self.cpu_usage_ax.set_ylabel('Usage (%)')
-        self.cpu_freq_ax.set_ylabel('Frequency (MHz)')
-
-        self.cpu_usage_ax.legend(loc='upper left')
-        self.cpu_freq_ax.legend(loc='upper right')
+        self.ax.set_xlabel('Time')
+        self.ax.set_ylabel(self.ylabel)
+        self.ax.legend(loc='upper left')
 
         self.canvas = FigureCanvasTkAgg(self.fig, self)
         #creating matplotlib toolbar
@@ -81,44 +85,20 @@ class GraphFrame(tk.Frame):
         # button1.pack(side='bottom')
         #place the canvas on the tkinter window
         self.canvas.get_tk_widget().pack(side='top', fill=tk.BOTH, expand=True)
-        self.update_data()
 
-    def get_network_usage(self):
-        new_value = psutil.net_io_counters(nowrap=True).bytes_sent + psutil.net_io_counters(nowrap=True).bytes_recv
-        net_usage = (new_value - self.old_network_value) / 1024.0 * 8
-        return (net_usage, new_value)
-
-    def update_data(self):
-        self.cpu_usage_data.append(psutil.cpu_percent())
-        self.cpu_freq_data.append(psutil.cpu_freq().current)
-        self.mem_usage_data.append(psutil.virtual_memory().percent)
-        # time_data.append(datetime.datetime.now().strftime("%H:%M:%S"))
-        self.time_data.append(datetime.datetime.now())
-        (current_net_usage, new_network_usage) = self.get_network_usage()
-        self.network_data.append(current_net_usage)
-        self.old_network_value = new_network_usage
 
     def animate(self):
         # while True:
-        self.update_data()
-        print(f'time: {self.time_data}; cpu_usage: {self.cpu_usage_data}')
-        self.cpu_usage_plot.set_xdata(self.time_data)
-        self.cpu_usage_plot.set_ydata(self.cpu_usage_data)
+        update_data(self.data_type)
 
-        self.mem_usage_plot.set_xdata(self.time_data)
-        self.mem_usage_plot.set_ydata(self.mem_usage_data)
+        # print(f'time: {self.time_data}; cpu_usage: {self.cpu_usage_data}')
+        self.plot.set_xdata(data["time"])
+        self.plot.set_ydata(data[self.data_type])
 
-        self.cpu_freq_plot.set_xdata(self.time_data)
-        self.cpu_freq_plot.set_ydata(self.cpu_freq_data)
 
-        self.network_usage_plot.set_xdata(self.time_data)
-        self.network_usage_plot.set_ydata(self.network_data)
-
-        self.cpu_freq_ax.set_xlim(self.time_data[0], self.time_data[-1])
-        self.cpu_usage_ax.set_xlim(self.time_data[0], self.time_data[-1])
-        self.mem_usage_ax.set_xlim(self.time_data[0], self.time_data[-1])
-        self.network_usage_ax.set_xlim(self.time_data[0], self.time_data[-1])
-        self.network_usage_ax.set_ylim(0, max(self.network_data)+1000)
+        self.ax.set_xlim(data["time"][0],data["time"][-1])
+        if self.ylim != 100:
+            self.ax.set_ylim(0, max(data[self.data_type])+1000)
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
@@ -128,7 +108,6 @@ class GraphFrame(tk.Frame):
         # time.sleep(0.5)
         #sau cu animate
         self.after(1000, self.animate)
-
 
 
 class MainApp(tk.Frame):
@@ -145,20 +124,56 @@ class MainApp(tk.Frame):
                                 text="Plot")
         self.plot_button.pack(side='bottom', pady=10)
 
+class ScrollableFrame(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
 
-# plt.ion()
-# graph = ResGraph()
-# graph.animate()
+        # Canvas for scrolling
+        canvas = tk.Canvas(self)
+        scrollbar = tk.Scrollbar(self, orient="vertical", command=canvas.yview)
+        self.scrollable_frame = tk.Frame(canvas)
+
+        # Update scroll region when resizing
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+        # Create window inside canvas
+        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        canvas.pack(side="left", fill="both", expand=True)
+        scrollbar.pack(side="right", fill="y")
+
 
 
 root = tk.Tk()
 root.title('ResMon')
 root.geometry('{}x{}'.format(1200, 600))
 root.resizable(False, False)
-root.pack_propagate(False)
+# root.pack_propagate(False)
 
-# MainApp(root, bg='grey', height=300).pack(side='top', fill='both', expand=False)
-graph_frame = GraphFrame(root)
-graph_frame.pack(fill=tk.BOTH, expand=True)
-graph_frame.animate()
+scrollable_frame = ScrollableFrame(root)
+scrollable_frame.pack(side="top", fill="both", expand=True)
+
+
+for _ in range(0, 5):
+    update_data()
+    time.sleep(1)
+
+cpu_graph = GraphFrame(scrollable_frame.scrollable_frame, "blue", "CPU USAGE", "CPU USAGE", 100, "CPU USAGE (%)", "cpu_usage")
+cpu_graph.grid(row=0, column=0, pady=20, sticky='ew')
+cpu_graph.animate()
+
+mem_graph = GraphFrame(scrollable_frame.scrollable_frame, "orange", "CPU USAGE", "CPU USAGE", 100, "CPU USAGE (%)", "mem_usage")
+mem_graph.grid(row=1, column=0, pady=20, sticky='ew')
+mem_graph.animate()
+
+
+# graph_frame.pack(fill=tk.BOTH, expand=True)
+# graph_frame.animate()
 root.mainloop()
